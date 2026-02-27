b55f19e6dec10ded284b2be2d4c81da6eb6ea85b
PYongju <wns82978@naver.com>
2026-02-27 19:31:37 +0900

    ㄹㅇ테스트

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# 허용할 주소 목록
+origins = [
+    "http://localhost:3000", # 로컬 테스트용
+    "https://proud-ocean-029307b00.1.azurestaticapps.net", # 배포된 리액트 주소 (반드시 본인 것으로 수정!)
+]
+
 # 중요: UI(Static Web Apps)에서 요청을 보낼 때 차단되지 않도록 CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://zealous-tree-0db734d00.6.azurestaticapps.net"], # UI 주소 입력
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # 모든 방식(GET, POST 등) 허용
+    allow_headers=["*"], # 모든 헤더 허용
 )
 
 class Comment(BaseModel):
