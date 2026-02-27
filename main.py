13efdee5282ad233fb00747078f93a0c159d004b
PYongju <wns82978@naver.com>
2026-02-27 15:26:16 +0900

    연결테스트

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 실습 중에는 모두 허용, 나중에는 UI 주소만!
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/test-data")
+async def recive_data(data:dict):
+    print(f"받은 데이터: {data}")
+    return {"status": "success", "received": data}
 
 @app.get("/")
 def read_root():
